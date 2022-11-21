20cb3fbcb7ec7461affbc0ecd5cf7d55bb71d7b8
irenako <108225581+irenako@users.noreply.github.com>
2022-11-21 11:26:46 +0200

    Chapter 2: Application Structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     parser: '@babel/eslint-parser'
   },
   rules: {
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
