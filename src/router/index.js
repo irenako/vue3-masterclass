e7cca35c1b6a7378f00f1f68b88f6914c9746f35
irenako <108225581+irenako@users.noreply.github.com>
2022-11-21 14:16:04 +0200

    Chapter 3: Routing&Components Basics

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,46 @@
+import PageHome from '@/components/PageHome'
+import PageThreadShow from '@/components/PageThreadShow'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import NotFound from '@/components/PageNotFound'
+import sourceData from '@/data.json'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: PageHome
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/thread/:id',
+    name: 'ThreadShow',
+    component: PageThreadShow,
+    props: true,
+    beforeEnter (to, from, next) {
+      // check if thread exists
+      const threadExists = sourceData.threads.find(thread => thread.id === to.params.id)
+      // if exists continue
+      if (threadExists) {
+        return next()
+      } else {
+        next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          // preserve existing query and hash
+          query: to.query,
+          hash: to.hash
+        })
+      }
+      // if doesnt exist redirect to not found
+    }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+export default createRouter({
+  history: createWebHistory(),
   routes
 })
-
-export default router
